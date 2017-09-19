b8f891003eeaa2efdb2dacf5887feb1566e10c64
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-09-19 13:07:26 -0300

    Agrego mgmt command sample de lectura de data json

---
+++ elastic_spike/apps/api/management/commands/read_datajson.py
@@ -0,0 +1,26 @@
+#! coding: utf-8
+import requests
+
+from requests.exceptions import MissingSchema, ConnectionError
+from django.core.management import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('catalog', type=str)
+
+    def handle(self, *args, **options):
+        catalog_url = options['catalog']
+
+        try:
+            response = requests.get(catalog_url)
+        except (MissingSchema, ConnectionError):
+            raise CommandError("URL inválida")
+
+        if response.status_code != 200:
+            error = "Catálogo no leído. Status code: {}"
+            raise CommandError(error.format(response.status_code))
+
+        if response.json():
+            print("Leído bien")
+            print("Dataset len: " + str(len(response.json()['dataset'])))
