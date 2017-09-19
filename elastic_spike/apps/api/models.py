3f0c044ab1ce1d29b6efe982872c5db56814b939
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-09-19 16:13:26 -0300

    Agrego field de ID

--- elastic_spike/apps/api/models.py
+++ elastic_spike/apps/api/models.py
@@ -17,5 +17,6 @@ class Distribution(models.Model):
 
 
 class Field(models.Model):
+    series_id = models.CharField(max_length=200)
     metadata = models.TextField()
     distribution = models.ForeignKey(to=Distribution, on_delete=models.CASCADE)
