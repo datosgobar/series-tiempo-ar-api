0a015c310d3a70f5c47d508b98ba9b69bc45456b
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-09-19 11:22:33 -0300

    Agrego modelos de metadatos

--- elastic_spike/apps/api/models.py
+++ elastic_spike/apps/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Catalog(models.Model):
+    metadata = models.TextField()
+
+
+class Dataset(models.Model):
+    metadata = models.TextField()
+    catalog = models.ForeignKey(to=Catalog, on_delete=models.CASCADE)
+
+
+class Distribution(models.Model):
+    metadata = models.TextField()
+    dataset = models.ForeignKey(to=Dataset, on_delete=models.CASCADE)
+    download_url = models.URLField()
+
+
+class Field(models.Model):
+    metadata = models.TextField()
+    distribution = models.ForeignKey(to=Distribution, on_delete=models.CASCADE)
+
