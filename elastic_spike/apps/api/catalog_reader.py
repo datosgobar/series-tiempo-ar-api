fdb7742093321c06a36a5f0c3c4ffc3c20b08bd9
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-09-21 10:39:51 -0300

    Optimizaciones de escritura de db

--- elastic_spike/apps/api/catalog_reader.py
+++ elastic_spike/apps/api/catalog_reader.py
@@ -1,7 +1,6 @@
 #! coding: utf-8
 import json
 
-
 from .models import Catalog, Dataset, Distribution, Field
 
 
@@ -28,22 +27,15 @@ class DistributionScrapper:
 
     def __init__(self):
         self.distributions = []
+        self.fields = []
 
     def run(self, catalog):
         datasets = catalog.pop('dataset', None)
         if not datasets:
             return
 
-        title = catalog.pop('title', None)
-        catalog_model = Catalog.objects.filter(title=title)
-        if not catalog_model:
-            catalog_model = Catalog(title=title)
-        else:
-            catalog_model = catalog_model[0]
-
-        catalog_model.metadata = json.dumps(catalog)
-        catalog_model.save()
-
+        catalog_model = self._catalog_model(catalog)
+        dataset_models = []
         for dataset in datasets:
             distributions = dataset.pop('distribution', None)
             if not distributions:
@@ -53,17 +45,33 @@ class DistributionScrapper:
             dataset_model = Dataset.objects.filter(title=title)
             if not dataset_model:
                 dataset_model = Dataset(title=title)
+                dataset_models.append(dataset_model)
             else:
                 dataset_model = dataset_model[0]
             dataset_model.metadata = json.dumps(dataset)
             dataset_model.catalog = catalog_model
-            dataset_model.save()
 
             for distribution in distributions:
                 fields = distribution.pop('field', None)
 
                 if fields and fields[0]['specialType'] == 'time_index':
                     self.save(dataset_model, distribution, fields)
+
+        Dataset.objects.bulk_create(dataset_models)
+        Distribution.objects.bulk_create(self.distributions)
+        Field.objects.bulk_create(self.fields)
+
+    @staticmethod
+    def _catalog_model(catalog):
+        title = catalog.pop('title', None)
+        catalog_model = Catalog.objects.filter(title=title)
+        if not catalog_model:
+            catalog_model = Catalog(title=title)
+        else:
+            catalog_model = catalog_model[0]
+        catalog_model.metadata = json.dumps(catalog)
+        catalog_model.save()
+        return catalog_model
 
     def save(self, dataset_model, distribution, fields):
         title = distribution.pop('title', None)
@@ -75,12 +83,10 @@ class DistributionScrapper:
                                           title=title,
                                           dataset=dataset_model,
                                           download_url=url)
-        distribution_model.save()
-        self.distributions.append(distribution)
-
+        self.distributions.append(distribution_model)
         for field in fields:
             series_id = field.pop('id')
             field = Field(metadata=json.dumps(field),
                           distribution=distribution_model,
                           series_id=series_id)
-            field.save()
+            self.fields.append(field)
