cc2cfe36655a1134d2d473c24d45fbc29bde8bd3
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-10-03 17:41:59 -0300

    Parametrizo URLs

--- elastic_spike/apps/api/query/elastic.py
+++ elastic_spike/apps/api/query/elastic.py
@@ -7,5 +7,18 @@ class ElasticInstance(object):
     elastic = Elasticsearch()
 
     @classmethod
-    def get(cls):
+    def get(cls, urls=None):
+        """Devuelve la instancia del cliente de Elasticsearch. Si se
+        espeficica el parámetro 'urls', inicializa un cliente nuevo con
+        conexión a las instancias en esas direcciones.
+        
+        Args:
+            urls (list): Lista de URLs a conectarse
+            
+        Returns:
+            Elasticsearch
+        """
+        if not urls:
+            return cls.elastic
+        cls.elastic = Elasticsearch(urls)
         return cls.elastic
