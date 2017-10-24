5fe62a5df0f6e2de1601610ab6ab07d82b2707ff
Lucas Lavandeira <lucaslavandeira@gmail.com>
2017-10-24 17:18:00 -0300

    Agrego pruebas de actualización de datos

--- series_tiempo_ar_api/apps/api/tests/reader_tests.py
+++ series_tiempo_ar_api/apps/api/tests/reader_tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from series_tiempo_ar.search import get_time_series_distributions
 from elasticsearch_dsl import Search
 
-from series_tiempo_ar_api.apps.api.models import Distribution
+from series_tiempo_ar_api.apps.api.models import Distribution, Field
 from series_tiempo_ar_api.apps.api.query.elastic import ElasticInstance
 from series_tiempo_ar_api.apps.api.query.catalog_reader import \
     Scraper, Indexer, DatabaseLoader
@@ -22,6 +22,15 @@ class ScrapperTests(TestCase):
         catalog = os.path.join(SAMPLES_DIR, 'full_ts_data.json')
         self.scrapper.run(catalog)
 
+        self.assertTrue(len(self.scrapper.distributions))
+
+    def test_missing_field(self):
+        """No importa que un field no esté en metadatos, se scrapea
+        igual, para obtener todas las series posibles
+        """
+
+        catalog = os.path.join(SAMPLES_DIR, 'missing_field.json')
+        self.scrapper.run(catalog)
         self.assertTrue(len(self.scrapper.distributions))
 
 
@@ -49,6 +58,25 @@ class IndexerTests(TestCase):
         results = Search(using=ElasticInstance.get(),
                          index=self.test_index).execute()
         self.assertTrue(len(results))
+
+    def test_missing_field_update(self):
+        """Al actualizar una distribución, si falta un field
+        previamente indexado, no se borran los datos anteriores
+        """
+
+        missing_field = '212.1_PSCIOS_ERS_0_0_22'
+        catalog = os.path.join(SAMPLES_DIR, 'missing_field.json')
+        distributions = get_time_series_distributions(catalog)
+        db_loader = DatabaseLoader(read_local=True)
+        db_loader.run(catalog, distributions)
+        self.indexer.run(distributions=db_loader.distribution_models)
+
+        results = Search(using=ElasticInstance.get(),
+                         index=self.test_index)\
+            .filter('match', series_id=missing_field).execute()
+
+        self.assertTrue(len(results))
+        self.assertTrue(Field.objects.filter(series_id=missing_field))
 
     @classmethod
     def tearDownClass(cls):
