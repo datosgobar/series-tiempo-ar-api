4022c4cf63c62c0094041b1bb96d653b3f854b5f
Leandro Gomez <lgomez@devartis.com>
2017-11-02 10:52:46 -0300

    Arreglo el script para tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-PYTHON_COMMAND=${PYTHON_SCRIPT:-python3}
 
-$PYTHON_COMMAND manage.py test --stop --with-coverage --cover-branches  --cover-inclusive --cover-package=series_tiempo_ar_api --settings=conf.settings.testing --exclude=settings --exclude=migrations
+python manage.py test --stop --with-coverage --cover-branches  --cover-inclusive --cover-package=series_tiempo_ar_api --settings=conf.settings.testing --exclude=settings --exclude=migrations
