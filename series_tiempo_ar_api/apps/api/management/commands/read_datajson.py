8f4f0e999a82fb7d1e9ba87a4135038c0e1fa155
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2017-11-23 10:54:19 -0300

    agrego parámetros en read_datajson para leer un sólo catálogo

--- series_tiempo_ar_api/apps/api/management/commands/read_datajson.py
+++ series_tiempo_ar_api/apps/api/management/commands/read_datajson.py
@@ -1,16 +1,29 @@
 #! coding: utf-8
 import requests
 import yaml
-from django.core.management import BaseCommand
+from django.core.management import BaseCommand, CommandError
 from pydatajson import DataJson
 
 from series_tiempo_ar_api.apps.api.indexing.catalog_reader import index_catalog
 
 CATALOGS_INDEX = 'https://raw.githubusercontent.com/datosgobar/libreria-catalogos/master/indice.yml'
+MISSING_ARG_MSG = u"URL y name deben ser especificados a la vez"
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--url', dest='url', type=unicode, default=None)
+        parser.add_argument('--name', dest='name', type=unicode, default=None)
+
     def handle(self, *args, **options):
+        url = options['url']
+        name = options['name']
+        if url or name:
+            if not name or not url:
+                raise CommandError(MISSING_ARG_MSG)
+            index_catalog(DataJson(url), name)
+            return
+
         catalogs = yaml.load(requests.get(CATALOGS_INDEX).text)
 
         for catalog_id, values in catalogs.items():
