8f87e7e15c62f06c471f1c6db5538d9f7c0e9575
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2017-11-23 10:48:27 -0300

    Restricción a db: hago field catalog identifier unique

--- series_tiempo_ar_api/apps/api/models.py
+++ series_tiempo_ar_api/apps/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Catalog(models.Model):
     title = models.CharField(max_length=2000)
-    identifier = models.CharField(max_length=200, default='sspm')
+    identifier = models.CharField(max_length=200, default='sspm', unique=True)
     metadata = models.TextField()
 
 
