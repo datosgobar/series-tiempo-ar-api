8c4b7743921218413691615a999c5c0cc45c9ef2
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2017-12-07 11:25:02 -0300

    Mejoro la muestra de analytics
        refs:#110

--- series_tiempo_ar_api/apps/api/admin.py
+++ series_tiempo_ar_api/apps/api/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
 from .models import Catalog, Dataset, Distribution, Field, Query
+
+
+class QueryAdmin(ModelAdmin):
+    list_display = ('timestamp', 'params',)
+
 
 admin.site.register(Catalog)
 admin.site.register(Dataset)
 admin.site.register(Distribution)
 admin.site.register(Field)
-admin.site.register(Query)
+admin.site.register(Query, QueryAdmin)
