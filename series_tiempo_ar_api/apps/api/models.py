7e52706f0f34d3fd431a36a150b7c94cbf956640
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2017-12-07 12:49:43 -0300

    Agrego campo de descripción a los modelos de Field

--- series_tiempo_ar_api/apps/api/models.py
+++ series_tiempo_ar_api/apps/api/models.py
@@ -48,6 +48,7 @@ class Distribution(models.Model):
 class Field(models.Model):
     series_id = models.CharField(max_length=200, unique=True)
     title = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
     metadata = models.TextField()
     distribution = models.ForeignKey(to=Distribution, on_delete=models.CASCADE)
 
