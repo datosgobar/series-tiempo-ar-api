451ff935159866ba4c1a421c442e666f84a20ddd
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2017-12-28 11:03:49 -0300

    Agrego vista de prueba para analytics de Kong
        refs:#123

--- series_tiempo_ar_api/apps/api/urls.py
+++ series_tiempo_ar_api/apps/api/urls.py
@@ -1,8 +1,9 @@
 #! coding: utf-8
 from django.conf.urls import url
 
-from series_tiempo_ar_api.apps.api.views import query_view
+from series_tiempo_ar_api.apps.api.views import query_view, save_request
 
 urlpatterns = [
-    url('^series/$', query_view, name='series')
+    url('^series/$', query_view, name='series'),
+    url('^analytics/$', save_request, name='save_request')
 ]
--- series_tiempo_ar_api/apps/api/views.py
+++ series_tiempo_ar_api/apps/api/views.py
@@ -1,8 +1,10 @@
 #! coding: utf-8
+from django.http import HttpResponse
 from ipware.ip import get_ip
 from series_tiempo_ar_api.apps.api.query import constants
 from .query.pipeline import QueryPipeline
 from .query.analytics import analytics
+from .models import Query
 
 
 def query_view(request):
@@ -18,3 +20,9 @@ def query_view(request):
         args_string = request.GET.urlencode()
         analytics.delay(ids, args_string, ip_address, args)
     return response
+
+
+def save_request(request):
+    query = Query(ids="test", args="test", ip_address="127.0.0.1", params=request.POST)
+    query.save()
+    return HttpResponse(request.POST)
