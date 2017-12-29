7db17fb87606a1104970f946254c9688a77cec08
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2017-12-29 12:15:43 -0300

    Agrego opcion de busqueda al panel de Query

--- series_tiempo_ar_api/apps/analytics/admin.py
+++ series_tiempo_ar_api/apps/analytics/admin.py
@@ -10,6 +10,7 @@ from series_tiempo_ar_api.apps.analytics.models import Query
 class QueryAdmin(ModelAdmin):
     list_display = ('timestamp', 'params',)
     readonly_fields = ('timestamp', 'params', 'ip_address', 'args', 'ids')
+    search_fields = ('timestamp', 'params', 'ip_address', 'args', 'ids')
 
 
 admin.site.register(Query, QueryAdmin)
