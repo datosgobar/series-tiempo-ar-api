54b45f941cb090ffc03f800fcced61edbf28b25f
Poli García <poli@devartis.com>
2018-01-02 15:03:52 -0300

    Ejecuto el script de deploy en el server de deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Ingresando al servidor remoto..."
-ssh -vvv $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "ls -lsa"
+ssh -vvv $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "cd ~/series-tiempo-ar-deploy && ansible-playbook -i inventories/testing/hosts --vault-password-file vault_pass.txt site.yml -vvv"
