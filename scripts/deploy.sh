e72336f25d07da6360a3544500ee1874965a825e
Poli García <poli@devartis.com>
2018-01-02 01:02:17 -0300

    Agrego logging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Copiando key..."
 echo $DEPLOY_TARGET_PUBKEY > "/tmp/$DEPLOY_TARGET_USERNAME.key"
+
+echo "Ingresando al servidor remoto..."
 ssh -vvv $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT -i "/tmp/$DEPLOY_TARGET_USERNAME.key" "ls -lsa"
+
+echo "Eliminando key..."
 rm "/tmp/$DEPLOY_TARGET_USERNAME.key"
