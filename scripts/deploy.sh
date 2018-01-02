1191a015a9818a0af06a853e43d605f301ed028e
Poli García <poli@devartis.com>
2018-01-02 15:12:55 -0300

    Activo el venv previo al deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Ingresando al servidor remoto..."
-ssh -vvv $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "cd ~/series-tiempo-ar-deploy && ansible-playbook -i inventories/testing/hosts --vault-password-file vault_pass.txt site.yml -vvv"
+echo "Ejecutando comando de instalación..."
+ssh $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "cd ~/series-tiempo-ar-deploy && source ./env/bin/activate && ansible-playbook -i inventories/testing/hosts --vault-password-file vault_pass.txt site.yml -vvv"
