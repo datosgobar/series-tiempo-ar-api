855b14f23be51bc8bc73a2a7e32ecfb37bb248bc
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2018-01-03 13:01:15 -0300

    Valido fechas iniciales y finales de los colapsos, asegurando que se calculan solo sobre períodos completos
        refs:#59
    
        refs:#59

--- series_tiempo_ar_api/apps/api/tests/helpers.py
+++ series_tiempo_ar_api/apps/api/tests/helpers.py
@@ -13,8 +13,44 @@ def setup_database():
     dataset = Dataset.objects.create(identifier="132",
                                      metadata='{}',
                                      catalog=catalog)
+    init_year_series(dataset)
+    init_semester_series(dataset)
+    init_month_series(dataset)
+    init_daily_series(dataset)
 
+
+def init_year_series(dataset):
     distrib = Distribution.objects.create(identifier='132.1',
+                                          metadata='{}',
+                                          download_url="invalid_url",
+                                          dataset=dataset,
+                                          periodicity='R/P1Y')
+    Field.objects.create(
+        series_id=settings.TEST_SERIES_NAME.format('year'),
+        metadata='{}',
+        distribution=distrib,
+        description="random series description",
+        title='random_year_0_title'
+    )
+
+
+def init_semester_series(dataset):
+    distrib = Distribution.objects.create(identifier='132.2',
+                                          metadata='{}',
+                                          download_url="invalid_url",
+                                          dataset=dataset,
+                                          periodicity='R/P6M')
+    Field.objects.create(
+        series_id=settings.TEST_SERIES_NAME.format('semester'),
+        metadata='{}',
+        distribution=distrib,
+        title='random_semester_0_title'
+    )
+
+
+def init_month_series(dataset):
+
+    distrib = Distribution.objects.create(identifier='132.3',
                                           metadata='{}',
                                           download_url="invalid_url",
                                           dataset=dataset,
@@ -26,12 +62,11 @@ def setup_database():
         description="random series description",
         title='random_month_0_title'
     )
-
-    init_daily_series(dataset)
+    return dataset
 
 
 def init_daily_series(dataset):
-    distrib = Distribution.objects.create(identifier='132.2',
+    distrib = Distribution.objects.create(identifier='132.4',
                                           metadata='{}',
                                           download_url="invalid_url",
                                           dataset=dataset,
@@ -41,20 +76,6 @@ def init_daily_series(dataset):
         metadata='{}',
         distribution=distrib,
         title='random_day_0_title'
-    )
-
-
-def init_semester_series(dataset):
-    distrib = Distribution.objects.create(identifier='132.3',
-                                          metadata='{}',
-                                          download_url="invalid_url",
-                                          dataset=dataset,
-                                          periodicity='R/P6M')
-    Field.objects.create(
-        series_id=settings.TEST_SERIES_NAME.format('semester'),
-        metadata='{}',
-        distribution=distrib,
-        title='random_semester_0_title'
     )
 
 
---
+++ series_tiempo_ar_api/apps/api/tests/incomplete_periods_tests.py
@@ -0,0 +1,131 @@
+#! coding: utf-8
+
+import pandas as pd
+from django.test import TestCase
+from random import random
+
+from nose.tools import raises
+
+from ..common import constants
+
+from ..common.incomplete_periods import handle_missing_values, handle_incomplete_value
+
+
+class IncompletePeriodsTests(TestCase):
+    length = 400
+
+    start_date = "2000-02-01"
+
+    def setUp(self):
+        self.col = pd.Series([random() for _ in range(self.length)])
+
+    def init_series(self, start_date, freq, target_freq):
+        idx = pd.DatetimeIndex(start=start_date, freq=freq,
+                               periods=self.length)
+        self.col.index = idx
+        transform_col = self.col.resample(target_freq).mean()
+
+        # Fix a colapsos fuera de fase:
+        if target_freq == constants.PANDAS_SEMESTER:
+            months_offset = transform_col.index[0].month - 1
+            if months_offset:
+                transform_col.drop(transform_col.index[0], inplace=True)
+            offset = pd.DateOffset(months=months_offset)
+            transform_col.index = transform_col.index - offset
+            transform_col.index.freq = constants.PANDAS_SEMESTER
+
+        return transform_col
+
+    def test_handle_semester_year(self):
+        transform_col = self.init_series("2000-02-01",
+                                         constants.PANDAS_SEMESTER,
+                                         constants.PANDAS_YEAR)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_quarter_year(self):
+        transform_col = self.init_series("2000-02-01",
+                                         constants.PANDAS_QUARTER,
+                                         constants.PANDAS_YEAR)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_quarter_semester(self):
+        transform_col = self.init_series("2000-04-01",
+                                         constants.PANDAS_QUARTER,
+                                         constants.PANDAS_SEMESTER)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_month_year(self):
+        transform_col = self.init_series("2000-04-01",
+                                         constants.PANDAS_MONTH,
+                                         constants.PANDAS_YEAR)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_month_semester(self):
+        transform_col = self.init_series("2000-06-01",
+                                         constants.PANDAS_MONTH,
+                                         constants.PANDAS_SEMESTER)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_month_quarter(self):
+        transform_col = self.init_series("2000-05-01",
+                                         constants.PANDAS_MONTH,
+                                         constants.PANDAS_QUARTER)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_day_year(self):
+        transform_col = self.init_series("2000-06-05",
+                                         constants.PANDAS_DAY,
+                                         constants.PANDAS_YEAR)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_day_month(self):
+        transform_col = self.init_series("2000-06-05",
+                                         constants.PANDAS_DAY,
+                                         constants.PANDAS_MONTH)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_day_semester(self):
+        transform_col = self.init_series("2000-06-05",
+                                         constants.PANDAS_DAY,
+                                         constants.PANDAS_SEMESTER)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    def test_handle_day_quarter(self):
+        transform_col = self.init_series("2000-06-05",
+                                         constants.PANDAS_DAY,
+                                         constants.PANDAS_QUARTER)
+
+        prev_length = len(transform_col)
+        handle_missing_values(self.col, transform_col)
+        self.assertEqual(len(transform_col), prev_length - 2)
+
+    @raises(ValueError)
+    def test_handle_missing_value_invalid_param(self):
+        handle_incomplete_value(self.col, which='invalid')
