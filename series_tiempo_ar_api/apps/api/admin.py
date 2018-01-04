b0a2364838764c77c0e8746ecbe9d9bd12aa7096
Lucas Lavandeira <lucaslavandeira@users.noreply.github.com>
2018-01-04 12:11:14 -0300

    Agrego acciones para marcar datasets como indexado o no al admin
        refs:#134

--- series_tiempo_ar_api/apps/api/admin.py
+++ series_tiempo_ar_api/apps/api/admin.py
@@ -3,7 +3,22 @@ from django.contrib import admin
 from .models import Catalog, Dataset, Distribution, Field
 
 
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'catalog', 'indexable')
+    search_fields = ('identifier', 'catalog__identifier', 'indexable')
+    readonly_fields = ('identifier', 'catalog')
+    actions = ['make_indexable', 'make_unindexable']
+
+    def make_unindexable(self, _, queryset):
+        queryset.update(indexable=False)
+    make_unindexable.short_description = 'Marcar como no indexable'
+
+    def make_indexable(self, _, queryset):
+        queryset.update(indexable=True)
+    make_indexable.short_description = 'Marcar como indexable'
+
+
 admin.site.register(Catalog)
-admin.site.register(Dataset)
+admin.site.register(Dataset, DatasetAdmin)
 admin.site.register(Distribution)
 admin.site.register(Field)
