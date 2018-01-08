3f16d3798173a1af5228936ee38c9a426270a0ab
Leandro Gomez <leandro.gz73@gmail.com>
2018-01-08 15:57:58 -0300

    Agrego unos comentarios
        refs:#137

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e;
 # Nota: Las variables no definidas aqui deben ser seteadas en ./variables.sh
 
+# TODO: Mejorar esta script.
 echo "Ejecutando comando de instalación..."
 ssh $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "\
     cd ~/series-tiempo-ar-deploy &&\
