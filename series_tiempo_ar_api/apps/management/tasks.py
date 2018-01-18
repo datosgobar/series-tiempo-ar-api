cb9090bec61b6ff685f48ba40a0041b2e14dfd30
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-01-18 12:56:14 -0300

    Borro catálogo fallido de la lista de nodos de las tareas

--- series_tiempo_ar_api/apps/management/tasks.py
+++ series_tiempo_ar_api/apps/management/tasks.py
@@ -36,6 +36,7 @@ def read_datajson(task, async=True):
                 start_index_catalog.delay(catalog_id, catalog_url, task_id)
         except (IOError, ValueError, AssertionError) as e:  # Errores que tira DataJson
             logs.append(READ_ERROR.format(catalog_id, e))
+            task.catalogs.remove(node)
 
         # Logging de errores de lectura
         logs_string = ''
