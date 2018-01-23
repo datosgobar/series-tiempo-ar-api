216a402eadc2b26b285985fa52fefb6aaf411438
Leandro Gomez <lgomez@devartis.com>
2018-01-23 15:57:09 -0300

    Agrego mas log al script de deployment

--- scripts/deploy/run_deploy.sh
+++ scripts/deploy/run_deploy.sh
@@ -7,6 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ENVIRONMENT="$1"
 
 # Cargo variables dependiendo del ambiente
+echo "Inicializando"
 . "$DIR/variables.sh" "$ENVIRONMENT"
+echo "Setup"
 "$DIR/prepare.sh"
+echo "Deploying"
 "$DIR/deploy.sh" "$ENVIRONMENT"
