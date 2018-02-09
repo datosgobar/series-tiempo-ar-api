a5ad50d4b9553c7e65f07f79d6d9045bd194d395
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-02-09 12:44:41 -0300

    Mockeo la ejecución de la búsqueda de metadatos

--- series_tiempo_ar_api/apps/metadata/tests/view_tests.py
+++ series_tiempo_ar_api/apps/metadata/tests/view_tests.py
@@ -2,15 +2,18 @@
 
 import json
 
+import mock
 from django.test import TestCase
 from django.urls import reverse
+from elasticsearch_dsl.search import Search
 
 
 class ViewTests(TestCase):
 
     def test_response_format(self):
-        response = self.client.get(reverse('metadata:search'),
-                                   data={'q': 'algodon'})
+        with mock.patch.object(Search, 'execute', return_value=[]):
+            response = self.client.get(reverse('metadata:search'),
+                                       data={'q': 'algodon'})
 
         response_json = json.loads(response.content)
         self.assertIn('data', response_json)
