cdd811094b6f98676e5c0064d1147c2ee1af5e05
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-02-22 12:59:58 -0300

    Borro clase no usada
        refs:#173
    
        refs:#173

--- series_tiempo_ar_api/apps/api/admin.py
+++ series_tiempo_ar_api/apps/api/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-
 from .models import Catalog, Dataset, Distribution, Field
-
-
-class MyAdmin(admin.AdminSite):
-    pass
 
 
 class DatasetAdmin(admin.ModelAdmin):
