bfa3b235db6f7e672631751f1d58c2b74dc8cf63
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-03-16 12:38:02 -0300

    Fix pylint

--- series_tiempo_ar_api/libs/indexing/tasks.py
+++ series_tiempo_ar_api/libs/indexing/tasks.py
@@ -4,7 +4,6 @@ import json
 from django.conf import settings
 from django_rq import job, get_queue
 from pydatajson import DataJson
-from redis import Redis
 
 from series_tiempo_ar_api.apps.management.models import ReadDataJsonTask, Node, Indicator
 from series_tiempo_ar_api.apps.api.models import Dataset, Catalog
@@ -22,31 +21,12 @@ def index_distribution(distribution_id, node_id, task,
     node = Node.objects.get(id=node_id)
     catalog = DataJson(json.loads(node.catalog))
     distribution = catalog.get_distribution(identifier=distribution_id)
-    catalog_model, created = Catalog.objects.get_or_create(identifier=node.catalog_id)
-    if created:
-        IndicatorLoader().increment_indicator(node.catalog_id, Indicator.CATALOG_NEW)
-        catalog_model.title = catalog['title']
-        catalog_model.identifier = node.catalog_id
-        catalog_model.save()
-
-    dataset_model, created = Dataset.objects.get_or_create(
-        identifier=distribution['dataset_identifier'],
-        catalog=catalog_model,
-    )
-
-    if created:
-        IndicatorLoader().increment_indicator(node.catalog_id, Indicator.DATASET_NEW)
-        dataset_model.indexable = whitelist
-        dataset_model.metadata = '{}'
-
-    dataset_model.present = True
-    dataset_model.save()
+    dataset_model = get_dataset(catalog, node.catalog_id, distribution['dataset_identifier'], whitelist)
     if not dataset_model.indexable:
         return
 
     try:
-        scraper = Scraper(read_local)
-        result = scraper.run(distribution, catalog)
+        result = Scraper(read_local).run(distribution, catalog)
         if not result:
             return
 
@@ -66,6 +46,29 @@ def index_distribution(distribution_id, node_id, task,
 
         if settings.RQ_QUEUES['indexing'].get('ASYNC', True):
             raise e  # Django-rq / sentry logging
+
+
+def get_dataset(catalog, catalog_id, dataset_id, whitelist):
+    catalog_model, created = Catalog.objects.get_or_create(identifier=catalog_id)
+    if created:
+        IndicatorLoader().increment_indicator(catalog_id, Indicator.CATALOG_NEW)
+        catalog_model.title = catalog['title']
+        catalog_model.identifier = catalog_id
+        catalog_model.save()
+
+    dataset_model, created = Dataset.objects.get_or_create(
+        identifier=dataset_id,
+        catalog=catalog_model,
+    )
+
+    if created:
+        IndicatorLoader().increment_indicator(catalog_id, Indicator.DATASET_NEW)
+        dataset_model.indexable = whitelist
+        dataset_model.metadata = '{}'
+
+    dataset_model.present = True
+    dataset_model.save()
+    return dataset_model
 
 
 # Para correr con el scheduler
