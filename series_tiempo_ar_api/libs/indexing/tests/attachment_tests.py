4e78f33aa5e7956fa97323c77f248e307d84bbf5
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-04-10 14:21:40 -0300

    Agrego más casos de prueba de attachments
        refs:#233

--- series_tiempo_ar_api/libs/indexing/tests/attachment_tests.py
+++ series_tiempo_ar_api/libs/indexing/tests/attachment_tests.py
@@ -10,7 +10,7 @@ from django.core.management import call_command
 from pydatajson import DataJson
 
 from series_tiempo_ar_api.apps.management.models import Node
-from series_tiempo_ar_api.apps.api.models import Catalog
+from series_tiempo_ar_api.apps.api.models import Catalog, Dataset, Distribution, Field
 from series_tiempo_ar_api.libs.indexing.report import attachments
 
 SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'samples')
@@ -44,3 +44,46 @@ class AttachmentTests(TestCase):
         self.assertTrue(row[0], catalog.identifier)
         self.assertTrue(row[1], catalog.title)
         self.assertTrue(row[4], str(self.node.indexable))
+
+    def test_dataset(self):
+        out = attachments.generate_dataset_attachment()
+
+        stream = StringIO(out)
+        reader = unicodecsv.reader(stream)
+        reader.next()  # Skip header
+
+        for row in reader:
+            dataset = Dataset.objects.get(identifier=row[0])
+
+            self.assertEqual(dataset.indexable, row[4])
+            self.assertEqual(dataset.present, row[5])
+            self.assertEqual(dataset.error, row[6])
+
+    def test_distribution(self):
+        out = attachments.generate_distribution_attachment()
+
+        stream = StringIO(out)
+        reader = unicodecsv.reader(stream)
+        reader.next()  # Skip header
+
+        for row in reader:
+            distribution = Distribution.objects.get(identifier=row[0])
+
+            self.assertEqual(distribution.indexable, row[4])
+            self.assertEqual(distribution.dataset.present, row[5])
+            self.assertEqual(distribution.error, row[6])
+
+    def test_fields(self):
+
+        out = attachments.generate_distribution_attachment()
+
+        stream = StringIO(out)
+        reader = unicodecsv.reader(stream)
+        reader.next()  # Skip header
+
+        for row in reader:
+            field = Field.objects.get(identifier=row[0])
+
+            self.assertEqual(field.distribution.dataset.indexable, row[4])
+            self.assertEqual(field.distribution.dataset.present, row[5])
+            self.assertEqual(field.distribution.error, row[6])
