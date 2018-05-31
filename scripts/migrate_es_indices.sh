3381b2a82602a60bc3cfbb47af98e517db274367
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-05-31 16:22:40 -0300

    Agrego un script de migración de indices de ES
    Solo por si es necesaria una migración manual en algún ambiente

---
+++ scripts/migrate_es_indices.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+NEW_MAPPING='
+{
+"mappings" : {
+  "doc" : {
+    "dynamic" : "strict",
+    "_all" : {
+      "enabled" : false
+    },
+    "properties" : {
+      "aggregation" : {
+        "type" : "keyword"
+      },
+      "catalog" : {
+        "type" : "keyword"
+      },
+      "change" : {
+        "type" : "scaled_float",
+        "scaling_factor" : 1.0E7
+      },
+      "change_a_year_ago" : {
+        "type" : "scaled_float",
+        "scaling_factor" : 1.0E7
+      },
+      "interval" : {
+        "type" : "keyword"
+      },
+      "percent_change" : {
+        "type" : "scaled_float",
+        "scaling_factor" : 1.0E7
+      },
+      "percent_change_a_year_ago" : {
+        "type" : "scaled_float",
+        "scaling_factor" : 1.0E7
+      },
+      "raw_value" : {
+        "type" : "boolean"
+      },
+      "series_id" : {
+        "type" : "keyword"
+      },
+      "timestamp" : {
+        "type" : "date"
+      },
+      "value" : {
+        "type" : "scaled_float",
+        "scaling_factor" : 1.0E7
+      }
+    }
+  }
+}
+}
+'
+
+TMP_INDEX="indicators_tmp"
+
+curl $ES_URL/${TMP_INDEX}/ -XPUT -d"${NEW_MAPPING}"
+
+REINDEX='{
+    "source": {"index": "indicators"},
+    "dest": {"index": "'${TMP_INDEX}'"}
+}';
+
+echo $REINDEX
+curl ${ES_URL}/_reindex/ -XPOST -d"${REINDEX}";
+
+curl ${ES_URL}/indicators/ -XDELETE
+
+curl $ES_URL/indicators/ -XPUT -d"${NEW_MAPPING}"
+
+REVERSE_REINDEX='{
+    "source": {"index": "'${TMP_INDEX}'"},
+    "dest": {"index": "indicators"}
+}';
+
+echo $REVERSE_REINDEX
+
+curl $ES_URL/_reindex -XPOST -d"${REVERSE_REINDEX}";
