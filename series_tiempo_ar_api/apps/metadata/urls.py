e1595d7c4c395ad976816fd68e5f2f95d017d912
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-05-31 15:23:54 -0300

    Corrijo urls de metadata

--- series_tiempo_ar_api/apps/metadata/urls.py
+++ series_tiempo_ar_api/apps/metadata/urls.py
@@ -5,8 +5,8 @@ from .views import search, dataset_source, field_units, dataset_publisher_name, 
 
 urlpatterns = [
     url('^$', search, name='search'),
-    url('^dataset_source$', dataset_source, name='dataset_source'),
-    url('^field_units$', field_units, name='field_units'),
-    url('^dataset_publisher_name', dataset_publisher_name, name='dataset_publisher_name'),
-    url('^dataset_theme$', dataset_theme, name='dataset_theme'),
+    url('^dataset_source/$', dataset_source, name='dataset_source'),
+    url('^field_units/$', field_units, name='field_units'),
+    url('^dataset_publisher_name/$', dataset_publisher_name, name='dataset_publisher_name'),
+    url('^dataset_theme/$', dataset_theme, name='dataset_theme'),
 ]
