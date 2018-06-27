93d79ed501210035f04b9c83666a843310d549b1
Poli García <poli@devartis.com>
2018-06-27 15:22:01 -0300

    Actualización de rutas de recursos estáticos

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -64,7 +64,7 @@ MEDIA_ROOT = str(APPS_DIR('media'))
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/series/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -74,7 +74,7 @@ STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/series/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
