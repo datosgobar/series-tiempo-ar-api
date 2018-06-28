b2f7ead124368d7ddfce243f236884e9979d9c21
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-06-28 14:57:11 -0300

    Muevo inicialización de ES en metadatos

--- series_tiempo_ar_api/apps/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-from series_tiempo_ar_api.libs.indexing.elastic import ElasticInstance
-
-es_configurations = settings.ES_CONFIGURATION
-urls = es_configurations["ES_URLS"]
-client_options = es_configurations["CONNECTIONS"]["default"]
-ElasticInstance.init(urls, client_options)
