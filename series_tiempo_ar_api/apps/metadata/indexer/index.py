e881f97b506338196318ce6e77e3dffdaa56662c
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-08-13 12:37:59 -0300

    Casteo queryset a list

--- series_tiempo_ar_api/apps/metadata/indexer/index.py
+++ series_tiempo_ar_api/apps/metadata/indexer/index.py
@@ -13,7 +13,7 @@ def add_analyzer(index: Index):
     texto en español
     """
 
-    synonyms = Synonym.objects.values_list('terms', flat=True)
+    synonyms = list(Synonym.objects.values_list('terms', flat=True))
 
     filters = ['lowercase', 'asciifolding']
     if synonyms:
