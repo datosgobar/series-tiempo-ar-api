c11ec0df76bf7e841e5f9c0dac028f8c85402625
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-08-16 11:38:06 -0300

    Reemplazo theme ids por objeto con más info

---
+++ series_tiempo_ar_api/apps/api/tests/metadata_tests.py
@@ -0,0 +1,26 @@
+#! coding: utf-8
+import json
+from django.test import TestCase
+from django_datajsonar.models import Field
+
+from series_tiempo_ar_api.apps.api.tests.helpers import get_series_id
+from series_tiempo_ar_api.apps.api.query.metadata_response import MetadataResponse
+
+
+class MetadataResponseTests(TestCase):
+    single_series = get_series_id('month')
+
+    def setUp(self):
+        self.field = Field.objects.get(identifier=self.single_series)
+
+    def test_theme_labels(self):
+        meta = json.loads(self.field.distribution.dataset.metadata)
+        meta.update({'theme': ['theme_id']})
+        self.field.distribution.dataset.metadata = json.dumps(meta)
+        self.field.distribution.dataset.themes = json.dumps([{'id': 'theme_id', 'label': 'test_label'}])
+        self.field.distribution.dataset.save()
+
+        full_meta = MetadataResponse(self.field).get_full_metadata()
+
+        self.assertTrue(isinstance(full_meta['dataset']['theme'], list))
+        self.assertEqual(full_meta['dataset']['theme'][0]['label'], 'test_label')
