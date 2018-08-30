25f84cf35bd14ebc0e908f85c7097627bbc6dc43
lucas <19612265+lucaslavandeira@users.noreply.github.com>
2018-08-30 15:11:51 -0300

    Optimizo admin view de Query

--- series_tiempo_ar_api/apps/analytics/admin.py
+++ series_tiempo_ar_api/apps/analytics/admin.py
@@ -2,19 +2,50 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.core.paginator import Paginator
+from django.db import connection
 from solo.admin import SingletonModelAdmin
 
 from .models import Query, ImportConfig, AnalyticsImportTask
 from .tasks import import_analytics_from_api_mgmt
 
 
+class LargeTablePaginator(Paginator):
+    """
+    Source: https://medium.com/squad-engineering/estimated-counts-for-faster-django-admin-change-list-963cbf43683e
+    Warning: Postgresql only hack
+    Overrides the count method of QuerySet objects to get an estimate instead of actual count when not filtered.
+    However, this estimate can be stale and hence not fit for situations where the count of objects actually matter.
+    """
+
+    def _get_count(self):
+        if getattr(self, '_count', None) is not None:
+            return self._count
+
+        query = self.object_list.query
+        if not query.where:
+            try:
+                cursor = connection.cursor()
+                cursor.execute("SELECT reltuples FROM pg_class WHERE relname = %s",
+                               [query.model._meta.db_table])
+                self._count = int(cursor.fetchone()[0])
+            except Exception as _:
+                self._count = super(LargeTablePaginator, self)._get_count()
+        else:
+            self._count = super(LargeTablePaginator, self)._get_count()
+
+        return self._count
+
+    count = property(_get_count)
+
+
 class QueryAdmin(admin.ModelAdmin):
-    date_hierarchy = 'timestamp'
+    # Impide hacer un COUNT(*) adicional en el armado de respuesta
+    show_full_result_count = False
+
+    paginator = LargeTablePaginator
 
     list_display = ('timestamp', 'status_code', 'uri', 'ip_address', 'params',)
-    list_filter = ('status_code', 'uri')
-
-    search_fields = ('timestamp', 'uri', 'status_code', 'params', 'ip_address', 'args', 'ids')
 
     def get_readonly_fields(self, request, obj=None):
         return [field.name for field in self.opts.local_fields]
