105588d630de80f56d21c4e473c0138ad0866296
--global <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-12 16:41:14 -0300

    Aumebnto el timeout de generación de dump
    timeout=0 lo toma como default de 180 segundos, lo subo a 1 hora (a futuro
    se van a generar varios dumps sobre este mismo proceso)

--- series_tiempo_ar_api/apps/dump/tasks.py
+++ series_tiempo_ar_api/apps/dump/tasks.py
@@ -7,7 +7,7 @@ from .models import CSVDumpTask
 from .generator.generator import DumpGenerator
 
 
-@job('default', timeout=0)
+@job('default', timeout='1h')
 def dump_db_to_csv(task_id):
     task = CSVDumpTask.objects.get(id=task_id)
     try:
