a436426f44918ba18f8456fdd7bcef8ec820dba9
--global <19612265+lucaslavandeira@users.noreply.github.com>
2018-09-18 15:46:40 -0300

    Devuelvo archivos de dump a través de nginx
    Si la variable MINIO_SERVE_FILES_URL está seteada en settings, uso ese
    path como base de la respuesta. Es necesario pasar por nginx para
    acceder a minio porque el port de minio no está expuesto a la red
    externa. nginx hace de proxy entre el user y minio

--- series_tiempo_ar_api/apps/dump/views.py
+++ series_tiempo_ar_api/apps/dump/views.py
@@ -1,6 +1,8 @@
 #!coding:utf-8
+from urllib.parse import urlparse
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
 from minio_storage.storage import create_minio_client_from_settings
 
 from .models import DumpFile
@@ -18,5 +20,10 @@ def serve_global_dump(_, filename):
     conn = create_minio_client_from_settings()
     response = conn.presigned_get_object(settings.MINIO_STORAGE_MEDIA_BUCKET_NAME,
                                          dump_file.file.name)
+
+    # Devuelvo el archivo desde nginx, de estar configurado
+    if getattr(settings, 'MINIO_SERVE_FILES_URL', None):
+        parsed = urlparse(response)
+        return redirect(f'{settings.MINIO_SERVE_FILES_URL}{parsed.path}?{parsed.query}')
 
     return HttpResponseRedirect(response)
