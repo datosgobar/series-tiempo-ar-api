9c941dc967462fe188b0e6012162664decb6726e
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-10-12 12:30:22 -0300

    Refactor de generación dumps XLSX
    Extraigo la lógica en varias clases y objetos distintos. Agrega la
    lógica necesaria para separar los dumps de valores y de metadatos según
    su frecuencia en hojas separadas.

--- series_tiempo_ar_api/apps/dump/generator/xlsx/generator.py
+++ series_tiempo_ar_api/apps/dump/generator/xlsx/generator.py
@@ -1,27 +1,32 @@
 import os
 
 from django.core.files import File
-from xlsxwriter.workbook import Workbook
 
 from series_tiempo_ar_api.apps.dump.generator.generator import remove_old_dumps
+from series_tiempo_ar_api.apps.dump.generator.xlsx.workbook import DumpWorkbook
 from series_tiempo_ar_api.apps.dump.models import DumpFile, GenerateDumpTask
 from series_tiempo_ar_api.utils import read_file_as_csv
 
 
 class XLSXWriter:
-    frequency_col = 'indice_tiempo_frecuencia'
+    multiple_sheets = {
+        DumpFile.FILENAME_VALUES: True,
+        DumpFile.FILENAME_FULL: True,
+        DumpFile.FILENAME_METADATA: False,
+        DumpFile.FILENAME_SOURCES: False,
+    }
 
-    def __init__(self, task: GenerateDumpTask, dump_file: DumpFile):
+    def __init__(self, task: GenerateDumpTask, dump_file: DumpFile, workbook_class=DumpWorkbook):
+        self.workbook_class = workbook_class
         self.task = task
         self.csv_dump_file = dump_file
-        self.header_row = None
-        self.frequency_index = None
+        self.frequency_column_index = None
         self.worksheets = {}
 
     def write(self):
         try:
             self.csv_to_xlsx()
-        except Exception as e:
+        except IOError as e:
             catalog = self.csv_dump_file.node or 'global'
             msg = f"Error escribiendo dump XLSX de dump {catalog} {self.csv_dump_file.file_name}: {e.__class__}: {e}"
             GenerateDumpTask.info(self.task, msg)
@@ -30,21 +35,17 @@ class XLSXWriter:
         """Escribe el dump en XLSX en un archivo temporal, luego lo guarda en el storage,
         por último borra el archivo temporal. Se debe hacer así porque """
         xlsx = self.xlsx_file_name()
-        workbook = Workbook(xlsx)
-        sheet_count = 0
         with self.csv_dump_file.file as f:
             reader = read_file_as_csv(f)
-            self.parse_header_row(next(reader))
-            for r, row in enumerate(reader):
-                if sheet_count > 1000000:
-                    sheet_count = 0
+            header_row = next(reader)
 
-                frequency = row[self.frequency_index]
-                for c, col in enumerate(row):
-                    worksheet = self.worksheets[frequency]
-                    worksheet['sheet'].write(worksheet['row'], c, col)
+            workbook = self.workbook_class(xlsx,
+                                           header_row=header_row,
+                                           split_by_frequency=self.multiple_sheets[self.csv_dump_file.file_name])
 
-                sheet_count += 1
+            for row in reader:
+                workbook.write_row(row)
+
         workbook.close()
 
         with open(xlsx, 'rb') as f:
@@ -58,32 +59,10 @@ class XLSXWriter:
     def xlsx_file_name(self):
         return f'{self.csv_dump_file.file_name}-{self.csv_dump_file.id}.{DumpFile.TYPE_XLSX}'
 
-    def parse_header_row(self, header: list):
-        self.header_row = header
-        for i, col_name in enumerate(self.header_row):
-            if col_name == self.frequency_col:
-                self.frequency_index = i
-                break
 
-    def init_worksheet(self, workbook: Workbook, frequency: str):
-        names = {
-            'R/P1Y': 'anual',
-            'R/P6M': 'semestral',
-            'R/P3M': 'trimestral',
-            'R/P1M': 'mensual',
-            'R/P1D': 'diaria',
-        }
-        worksheet = workbook.add_worksheet(names[frequency])
-        worksheet.write_row(self.header_row)
-        self.worksheets[frequency] = {
-            'sheet': worksheet,
-            'row': 0,
-        }
-        return worksheet
-
-def generate(task: GenerateDumpTask, node: str = None):
+def generate(task: GenerateDumpTask, node: str = None, workbook_class=DumpWorkbook):
     for dump in DumpFile.get_last_of_type(DumpFile.TYPE_CSV, node):
-        XLSXWriter(task, dump).write()
+        XLSXWriter(task, dump, workbook_class).write()
 
     for filename, _ in DumpFile.FILENAME_CHOICES:
         remove_old_dumps(filename, DumpFile.TYPE_CSV, node)
---
+++ series_tiempo_ar_api/apps/dump/generator/xlsx/workbook.py
@@ -0,0 +1,52 @@
+from xlsxwriter import Workbook
+
+from series_tiempo_ar_api.apps.dump.generator.xlsx.worksheet import DumpWorksheet, SingleWorksheet
+
+
+class DumpWorkbook:
+    frequency_col_name = 'indice_tiempo_frecuencia'
+
+    def __init__(self, filename: str, header_row: list, split_by_frequency=False):
+        self.workbook = Workbook(filename)
+        self.split_by_frequency = split_by_frequency
+
+        self.header_row = header_row
+        for i, col_name in enumerate(self.header_row):
+            if col_name == self.frequency_col_name:
+                self.frequency_column_index = i
+                break
+
+        self.sheets = {}
+        self.single_sheet = None
+
+    def add_worksheet(self, sheet_name, frequency):
+        self.sheets[frequency] = DumpWorksheet(self.workbook, sheet_name)
+        self.sheets[frequency].write_row(self.header_row)
+
+    def write_row(self, row):
+        if self.split_by_frequency:
+            sheet = row[self.frequency_column_index]
+            if sheet not in self.sheets:
+                self.init_worksheet(sheet)
+            self.sheets[sheet].write_row(row)
+            return
+
+        if not self.single_sheet:
+            self.single_sheet = SingleWorksheet(self.workbook)
+            self.single_sheet.write_row(self.header_row)
+
+        self.single_sheet.write_row(row)
+
+    def close(self):
+        self.workbook.close()
+
+    def init_worksheet(self, frequency: str):
+        names = {
+            'R/P1Y': 'anual',
+            'R/P6M': 'semestral',
+            'R/P3M': 'trimestral',
+            'R/P1M': 'mensual',
+            'R/P1D': 'diaria',
+        }
+        sheet_name = names[frequency]
+        self.add_worksheet(sheet_name, frequency)
