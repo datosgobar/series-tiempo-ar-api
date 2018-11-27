03bca4c8607b08b85ad06f0f488bfcea27ebadc5
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-11-27 14:37:13 -0300

    Agrego un par de features de utils al admin de dumps
    - cerrar tareas trabadas
    - borrar archivos de dumps junto con los modelos al ejecutar el delete

--- series_tiempo_ar_api/apps/dump/admin.py
+++ series_tiempo_ar_api/apps/dump/admin.py
@@ -1,18 +1,39 @@
 #!coding=utf8
+import os
+
 from django.contrib import admin, messages
+from django.db.models.query import QuerySet
+from django.utils import timezone
 
 from django_datajsonar.admin import AbstractTaskAdmin
 
 from series_tiempo_ar_api.apps.dump.tasks import enqueue_dump_task
-from .models import GenerateDumpTask
+from .models import GenerateDumpTask, DumpFile
 
 
 class GenerateDumpTaskAdmin(AbstractTaskAdmin):
     model = GenerateDumpTask
 
     list_display = ('__str__', 'file_type', 'status')
+    actions = ['delete_model', 'mark_finished']
 
     task = enqueue_dump_task
+
+    def get_actions(self, request):
+        actions = super(GenerateDumpTaskAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+
+        return actions
+
+    def delete_model(self, request, queryset):
+        obj: GenerateDumpTask
+        for obj in queryset:  # Fuerza el borrado de los archivos en el sistema
+            obj.delete()
+
+    def mark_finished(self, _, queryset: QuerySet):
+        queryset.update(status=self.model.FINISHED)
+        queryset.filter(finished=None).update(finished=timezone.now())
 
     def save_model(self, request, obj, form, change):
         if self.model.objects.filter(status=self.model.RUNNING, file_type=obj.file_type):
