b529912796f9f7a329c28a10c78132c93eed3a24
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2018-11-29 12:04:26 -0300

    Borrado de código no usado
    Saca toda la funcionalidad de armado de dump de analytics, la
    funcionalidad está provista ahora de manera más robusta por api-gateway.
    Saca el endpoint de save() de analytics, el cargado también es hecho por
    api-gateway

--- series_tiempo_ar_api/apps/analytics/tasks.py
+++ series_tiempo_ar_api/apps/analytics/tasks.py
@@ -1,44 +1,7 @@
 #! coding: utf-8
-import os
-import json
 import requests
-import unicodecsv
 from django_rq import job
-from django.conf import settings
-from django.utils import timezone
-
-from .models import Query
-from .utils import kong_milliseconds_to_tzdatetime
 from .importer import AnalyticsImporter
-
-
-@job("default")
-def analytics(ids, args_string, ip_address, params, timestamp_milliseconds):
-    params_json = json.dumps(params)
-    timestamp = kong_milliseconds_to_tzdatetime(timestamp_milliseconds)
-    query = Query(ids=ids, args=args_string, ip_address=ip_address, params=params_json, timestamp=timestamp)
-    query.save()
-
-
-@job("default", timeout=360)
-def export(path=None):
-    queryset = Query.objects.all()
-    filepath = path or os.path.join(settings.PROTECTED_MEDIA_DIR, settings.ANALYTICS_CSV_FILENAME)
-
-    fields = {
-        'timestamp': lambda x: timezone.localtime(x.timestamp),
-        'ip_address': lambda x: x.ip_address,
-        'ids': lambda x: x.ids,
-        'params': lambda x: x.params,
-    }
-
-    with open(filepath, 'wb') as f:
-        writer = unicodecsv.writer(f)
-        # header
-        writer.writerow([field for field in fields])
-        for query in queryset.iterator():
-
-            writer.writerow([val(query) for val in fields.values()])
 
 
 @job('default', timeout=1000)
