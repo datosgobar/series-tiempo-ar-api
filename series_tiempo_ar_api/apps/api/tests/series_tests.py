69793bb9392e7d04f969a5ef0fde4c395ec6ed94
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-01-17 15:49:23 -0300

    Corrijo nombres de variables
    serie -> series

--- series_tiempo_ar_api/apps/api/tests/series_tests.py
+++ series_tiempo_ar_api/apps/api/tests/series_tests.py
@@ -13,132 +13,132 @@ class SeriesStartOffsetTests(TestCase):
     def setUp(self):
         self.faker = Faker()
 
-    def get_serie(self, periodicity, rep_mode=None):
+    def get_series(self, periodicity, rep_mode=None):
         fake_index = self.faker.pystr().lower()
-        serie_id = self.faker.pystr()
+        series_id = self.faker.pystr()
         rep_mode = rep_mode or constants.API_DEFAULT_VALUES[constants.PARAM_REP_MODE]
         return Series(index=fake_index,
-                      series_id=serie_id,
+                      series_id=series_id,
                       rep_mode=rep_mode,
                       periodicity=periodicity)
 
     def test_series_id_same_start(self):
-        one_serie = self.get_serie('month')
-        other_serie = self.get_serie('month')
+        one_series = self.get_series('month')
+        other_series = self.get_series('month')
 
         start_date = datetime(2016, 1, 1)
-        start = one_serie.get_es_start({one_serie.series_id: start_date, other_serie.series_id: start_date}, 0)
+        start = one_series.get_es_start({one_series.series_id: start_date, other_series.series_id: start_date}, 0)
 
         self.assertEqual(start, 0)
 
     def test_series_id_one_month_earlier_start(self):
-        one_serie = self.get_serie('month')
-        other_serie = self.get_serie('month')
+        one_series = self.get_series('month')
+        other_series = self.get_series('month')
 
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(months=1)
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 1)
-        other_start = other_serie.get_es_start(start_dates, 1)
+        start = one_series.get_es_start(start_dates, 1)
+        other_start = other_series.get_es_start(start_dates, 1)
         self.assertEqual(start, 1)
         self.assertEqual(other_start, 0)
 
     def test_series_with_collapse(self):
-        one_serie = self.get_serie('month')
-        other_serie = self.get_serie('month')
+        one_series = self.get_series('month')
+        other_series = self.get_series('month')
 
-        one_serie.add_collapse('year')
-        other_serie.add_collapse('year')
+        one_series.add_collapse('year')
+        other_series.add_collapse('year')
 
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(months=1)
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 1)
-        other_start = other_serie.get_es_start(start_dates, 1)
+        start = one_series.get_es_start(start_dates, 1)
+        other_start = other_series.get_es_start(start_dates, 1)
         self.assertEqual(start, 1)
         self.assertEqual(other_start, 0)
 
     def test_series_different_periodicity(self):
-        one_serie = self.get_serie('year')
-        other_serie = self.get_serie('month')
+        one_series = self.get_series('year')
+        other_series = self.get_series('month')
 
-        other_serie.add_collapse('year')
+        other_series.add_collapse('year')
 
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(months=1)
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 1)
-        other_start = other_serie.get_es_start(start_dates, 1)
+        start = one_series.get_es_start(start_dates, 1)
+        other_start = other_series.get_es_start(start_dates, 1)
         self.assertEqual(start, 1)
         self.assertEqual(other_start, 0)
 
     def test_series_start_many_years_delay(self):
-        one_serie = self.get_serie('year')
-        other_serie = self.get_serie('year')
+        one_series = self.get_series('year')
+        other_series = self.get_series('year')
 
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(years=100)
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 50)
-        other_start = other_serie.get_es_start(start_dates, 50)
+        start = one_series.get_es_start(start_dates, 50)
+        other_start = other_series.get_es_start(start_dates, 50)
         self.assertEqual(start, 50)
         self.assertEqual(other_start, 0)
 
     def test_series_day_and_month_periodicity(self):
-        one_serie = self.get_serie('month')
-        other_serie = self.get_serie('day')
+        one_series = self.get_series('month')
+        other_series = self.get_series('day')
 
-        other_serie.add_collapse('month')
+        other_series.add_collapse('month')
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(days=1)
 
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 50)
-        other_start = other_serie.get_es_start(start_dates, 50)
+        start = one_series.get_es_start(start_dates, 50)
+        other_start = other_series.get_es_start(start_dates, 50)
 
         self.assertEqual(start, 50)
         self.assertEqual(other_start, 49)
 
     def test_series_day_and_quarter_periodicity(self):
-        one_serie = self.get_serie('quarter')
-        other_serie = self.get_serie('day')
+        one_series = self.get_series('quarter')
+        other_series = self.get_series('day')
 
-        other_serie.add_collapse('quarter')
+        other_series.add_collapse('quarter')
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(days=1)
 
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 50)
-        other_start = other_serie.get_es_start(start_dates, 50)
+        start = one_series.get_es_start(start_dates, 50)
+        other_start = other_series.get_es_start(start_dates, 50)
 
         self.assertEqual(start, 50)
         self.assertEqual(other_start, 49)
 
     def test_series_day_and_semester_periodicity(self):
-        one_serie = self.get_serie('semester')
-        other_serie = self.get_serie('day')
+        one_series = self.get_series('semester')
+        other_series = self.get_series('day')
 
-        other_serie.add_collapse('semester')
+        other_series.add_collapse('semester')
         one_start_date = datetime(2016, 1, 1)
         other_start_date = one_start_date + relativedelta(days=1)
 
-        start_dates = {one_serie.series_id: one_start_date,
-                       other_serie.series_id: other_start_date}
+        start_dates = {one_series.series_id: one_start_date,
+                       other_series.series_id: other_start_date}
 
-        start = one_serie.get_es_start(start_dates, 50)
-        other_start = other_serie.get_es_start(start_dates, 50)
+        start = one_series.get_es_start(start_dates, 50)
+        other_start = other_series.get_es_start(start_dates, 50)
 
         self.assertEqual(start, 50)
         self.assertEqual(other_start, 49)
