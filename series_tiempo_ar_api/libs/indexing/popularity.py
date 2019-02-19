2472a0818ee8f050355a5973c7b5fcc36b9a7f60
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-02-19 11:09:33 -0300

    Utilizo filters aggregation para popularidad
    Permite una composición de requests a ES, hacemos 1 request por
    distribución, por día de granularidad de la agregación (30, 90, 180,
    total), es decir, 4 requests por distribución, en vez de 4 por serie.

--- series_tiempo_ar_api/libs/indexing/popularity.py
+++ series_tiempo_ar_api/libs/indexing/popularity.py
@@ -1,7 +1,6 @@
 from django_datajsonar.models import Distribution
 from elasticsearch_dsl import Search, Q, Index
 from series_tiempo_ar_api.apps.management import meta_keys
-from series_tiempo_ar_api.apps.analytics.elasticsearch.index import SeriesQuery
 
 KEY_DAYS_PAIRS = (
     (meta_keys.HITS_TOTAL, None),
@@ -18,27 +17,37 @@ def update_popularity_metadata(distribution: Distribution):
     series = distribution.field_set\
         .exclude(identifier='indice_tiempo')\
         .exclude(identifier=None)
-    for serie in series:
-        for meta_key, days in KEY_DAYS_PAIRS:
-            hits = popularity_aggregation(serie.identifier, days)
+
+    series_ids = series.values_list('identifier', flat=True)
+
+    for meta_key, days in KEY_DAYS_PAIRS:
+        hits = popularity_aggregation(series_ids, days)
+
+        for serie in series:
+            serie_hits = hits[serie.identifier].count.value
             serie.enhanced_meta.update_or_create(key=meta_key,
-                                                 defaults={'value': hits})
+                                                 defaults={'value': serie_hits})
 
 
-def popularity_aggregation(serie_id: str, days: int) -> int:
-    filters = [Q('term', serie_id=serie_id)]
-    if days is not None:
-        filters.append(Q('range', timestamp={'gte': f'now-{days}d'}))
-
+def popularity_aggregation(series_ids: str, days: int):
     s = Search(index='query')
     s.aggs \
         .bucket(name="hits_last_days",
-                agg_type='filter',
-                bool={'filter': filters}) \
+                agg_type='filters',
+                filters={serie_id: get_serie_filter(serie_id, days) for serie_id in series_ids})\
         .metric(name='count',
                 agg_type='value_count',
                 field='serie_id')
     s = s[:0]
 
     result = s.execute()
-    return result.aggregations.hits_last_days.count.value
+    return result.aggregations.hits_last_days.buckets
+
+
+def get_serie_filter(serie_id: str, days: int) -> dict:
+    filters = []
+    if days is not None:
+        filters.append(Q('range', timestamp={'gte': f'now-{days}d'}))
+
+    filters.append(Q('term', serie_id=serie_id))
+    return {'bool': {'filter': filters}}
