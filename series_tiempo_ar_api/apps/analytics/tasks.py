276b3dc31df8c4ff4485e229ee587aa9f643d3d4
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-02-21 11:24:45 -0300

    Separo las colas de workers de analytics
    'analytics' para la tarea de importado, y 'hits_indicators' para el
    cálculo de indicadores de consultas desde el índice de Elasticsearch. La
    primer tarea indexa los datos, la segunda los consume

--- series_tiempo_ar_api/apps/analytics/tasks.py
+++ series_tiempo_ar_api/apps/analytics/tasks.py
@@ -8,7 +8,7 @@ from django_rq import job
 from series_tiempo_ar_api.apps.analytics.elasticsearch.indicators import calculate_hits_indicators
 from .importer import AnalyticsImporter
 
-
+@job('analytics')
 def enqueue_new_import_analytics_task(limit=1000, requests_lib=requests, import_all=False, index_to_es=True):
     AnalyticsImporter(limit=limit, requests_lib=requests_lib, index_to_es=index_to_es).run(import_all)
 
@@ -18,7 +18,7 @@ def import_analytics(task, limit=1000, requests_lib=requests, import_all=False, 
     AnalyticsImporter(task=task, limit=limit, requests_lib=requests_lib, index_to_es=index_to_es).run(import_all)
 
 
-@job('analytics', timeout=1000)
+@job('hits_indicators', timeout=1000)
 def enqueue_new_calculate_hits_indicators_task():
     yesterday = datetime.date.today() - relativedelta(days=1)
     calculate_hits_indicators(for_date=yesterday)
