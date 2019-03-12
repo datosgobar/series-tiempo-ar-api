d361f8c46a892d2f1b5c4eb30910058e65b53559
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-03-12 14:27:04 -0300

    Adapto scraper para usar DistributionRepository

--- series_tiempo_ar_api/libs/indexing/tests/scraper_tests.py
+++ series_tiempo_ar_api/libs/indexing/tests/scraper_tests.py
@@ -1,6 +1,7 @@
 #! coding: utf-8
 import os
 
+from unittest import mock
 from pydatajson import DataJson
 from django.test import TestCase
 from nose.tools import raises
@@ -24,13 +25,21 @@ class MockDistribution:
         self.dataset = self.Dataset(distribution[DATASET_IDENTIFIER])
 
 
+def get_mock_index_series():
+    index_series = mock.MagicMock()
+    index_series.title = 'indice_tiempo'
+    return index_series
+
+
+@mock.patch('series_tiempo_ar_api.libs.indexing.scraping.DistributionRepository.get_time_index_series',
+            return_value=get_mock_index_series())
 class ScrapperTests(TestCase):
     def setUp(self):
         self.task = ReadDataJsonTask()
         self.task.save()
         self.scrapper = Scraper(read_local=True)
 
-    def test_scrapper(self):
+    def test_scrapper(self, *_):
         catalog = DataJson(os.path.join(SAMPLES_DIR, 'full_ts_data.json'))
         distribution = catalog.get_distributions(only_time_series=True)[0]
 
@@ -39,7 +48,7 @@ class ScrapperTests(TestCase):
 
         self.assertTrue(result)
 
-    def test_missing_metadata_field(self):
+    def test_missing_metadata_field(self, *_):
         """No importa que un field no esté en metadatos, se scrapea
         igual, para obtener todas las series posibles
         """
@@ -52,7 +61,7 @@ class ScrapperTests(TestCase):
         self.assertTrue(result)
 
     @raises(Exception)
-    def test_missing_dataframe_column(self):
+    def test_missing_dataframe_column(self, *_):
         """Si falta una columna indicada por los metadatos, no se
         scrapea la distribución
         """
@@ -65,7 +74,7 @@ class ScrapperTests(TestCase):
         distribution = MockDistribution(distribution)
         self.scrapper.run(distribution, catalog)
 
-    def test_validate_all_zero_series(self):
+    def test_validate_all_zero_series(self, *_):
         catalog = DataJson(os.path.join(
             SAMPLES_DIR, 'ts_all_zero_series.json'
         ))
@@ -76,7 +85,7 @@ class ScrapperTests(TestCase):
         self.assertTrue(result)
 
     @raises(DistributionTooManyNullSeriesError)
-    def test_validate_all_null_series(self):
+    def test_validate_all_null_series(self, *_):
         catalog = DataJson(os.path.join(
             SAMPLES_DIR, 'ts_all_null_series.json'
         ))
