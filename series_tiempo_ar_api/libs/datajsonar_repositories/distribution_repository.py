a4c502e33b7d32fb0ce2e8f6dc419c1151905ab3
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-03-14 12:06:47 -0300

    No filtro por presentes para obtener el índice de tiempo
    Si la distribución está discontinuada, filtrar por presentes hacía que siempre falle el método

--- series_tiempo_ar_api/libs/datajsonar_repositories/distribution_repository.py
+++ series_tiempo_ar_api/libs/datajsonar_repositories/distribution_repository.py
@@ -11,7 +11,7 @@ class DistributionRepository:
         self.instance = instance
 
     def get_time_index_series(self):
-        fields = self.instance.field_set.filter(present=True)
+        fields = self.instance.field_set.all()
         for field in fields:
             meta = json.loads(field.metadata)
             if meta.get(constants.SPECIAL_TYPE) == constants.TIME_INDEX:
