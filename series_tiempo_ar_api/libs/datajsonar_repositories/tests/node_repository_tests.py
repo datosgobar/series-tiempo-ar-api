dc331ecd86c6651b2494e6f1c404a4815282b08e
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-03-26 14:03:05 -0300

    Uso mocks para node repository

--- series_tiempo_ar_api/libs/datajsonar_repositories/tests/node_repository_tests.py
+++ series_tiempo_ar_api/libs/datajsonar_repositories/tests/node_repository_tests.py
@@ -1,7 +1,7 @@
 import os
 
+import mock
 from django.test import TestCase
-from django_datajsonar.models import Node
 
 from series_tiempo_ar_api.libs.datajsonar_repositories.NodeRepository import NodeRepository
 
@@ -12,10 +12,7 @@ SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'samples')
 class NodeRepositoryTests(TestCase):
 
     def test_read_catalog_returns_catalog_with_distributions(self):
-
-        node = Node.objects.create(catalog_id='my catalog',
-                                   catalog_url=os.path.join(SAMPLES_DIR, 'test_catalog.json'),
-                                   indexable=True)
+        node = mock.Mock(catalog_url=os.path.join(SAMPLES_DIR, 'test_catalog.json'))
 
         catalog = NodeRepository(node).read_catalog()
         self.assertTrue(catalog.get_distributions())
