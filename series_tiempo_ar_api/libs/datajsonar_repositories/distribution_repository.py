88f917cfb779f984e162eeaf7bcd6e58534465d3
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-03-26 15:36:18 -0300

    Agrego get_node a DistributionRepository

--- series_tiempo_ar_api/libs/datajsonar_repositories/distribution_repository.py
+++ series_tiempo_ar_api/libs/datajsonar_repositories/distribution_repository.py
@@ -1,6 +1,6 @@
 import json
 
-from django_datajsonar.models import Distribution, Field
+from django_datajsonar.models import Distribution, Field, Node
 
 from series_tiempo_ar_api.libs.indexing import constants
 
@@ -19,3 +19,6 @@ class DistributionRepository:
 
         msg = f"No se encontró un índice de tiempo para la distribución: {self.instance.identifier}"
         raise Field.DoesNotExist(msg)
+
+    def get_node(self):
+        return Node.objects.get(catalog_id=self.instance.dataset.catalog.identifier)
--- series_tiempo_ar_api/libs/datajsonar_repositories/tests/distribution_repository_tests.py
+++ series_tiempo_ar_api/libs/datajsonar_repositories/tests/distribution_repository_tests.py
@@ -1,6 +1,7 @@
 import json
 
-from mock import Mock
+from django_datajsonar.models import Node
+from mock import Mock, patch
 from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 from nose.tools import raises
@@ -23,3 +24,13 @@ class DistributionRepositoryTests(TestCase):
         distribution = Mock()
         distribution.field_set.all.return_value = []
         DistributionRepository(distribution).get_time_index_series()
+
+    def test_get_node(self):
+        distribution = Mock()
+        distribution.dataset.catalog.identifier = 'test_node'
+
+        node = Node(catalog_id='test_node', catalog_url='test', indexable=True)
+        with patch('series_tiempo_ar_api.libs.datajsonar_repositories.distribution_repository.Node') as fake_node:
+            fake_node.objects.get.return_value = node
+            self.assertTrue(fake_node.objects.get.called_with(catalog_id='test_node'))
+            self.assertEqual(DistributionRepository(distribution).get_node(), node)
