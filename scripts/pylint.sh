9e4f5bc3d1e8c64a44231fdfa7f73808cd142e41
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-04-11 11:43:52 -0300

    Agrego opción para correr pylint solo sobre el diff

--- scripts/pylint.sh
+++ scripts/pylint.sh
@@ -5,5 +5,13 @@ DIR=$(dirname "$0")
 cd ${DIR}/..
 
 echo "Running pylint"
-pylint -f parseable series_tiempo_ar_api --rcfile=.pylintrc
+if [[ "$1" = "--full" ]]; then
+    pylint -f parseable series_tiempo_ar_api --rcfile=.pylintrc
+else
+    git diff --name-only origin/master |
+    grep '^series_tiempo_ar_api/.*\.py$' |
+    grep -v migrations |
+    grep -v settings |
+    xargs -r pylint --rcfile=.pylintrc -f parseable
+fi
 echo "pylint OK :)"
