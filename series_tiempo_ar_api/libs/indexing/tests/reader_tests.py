24d9068ff4bef147bd869dae2952fb6a0b256f2a
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-02 14:57:38 -0300

    Agrego casos de prueba de índices parciales
    Contempla un soporte al menos incipiente para casos de índices de tiempo tipo ISO-8601 parcial, del estilo:
    YYYY-MM para series mensuales, trimestrales o semestrales, y YYYY para series anuales.

--- series_tiempo_ar_api/libs/indexing/tests/reader_tests.py
+++ series_tiempo_ar_api/libs/indexing/tests/reader_tests.py
@@ -2,22 +2,16 @@
 import os
 import mock
 
-from django.core.files import File
-from django.db import transaction
+from django.db.models import Q
 from django.test import TestCase
-from elasticsearch_dsl import Search
-from elasticsearch_dsl.connections import connections
 
 from django_datajsonar.tasks import read_datajson
 from django_datajsonar.models import Distribution, Field, Catalog
 from django_datajsonar.models import ReadDataJsonTask, Node
 
-from series_tiempo_ar_api.libs.datajsonar_repositories.series_repository import SeriesRepository
-from series_tiempo_ar_api.libs.indexing.indexer.data_frame import init_df
 from series_tiempo_ar_api.apps.management import meta_keys
 from series_tiempo_ar_api.apps.management.models import IndexDataTask as ManagementTask
 from series_tiempo_ar_api.libs.indexing.catalog_reader import index_catalog
-from series_tiempo_ar_api.libs.indexing.indexer.distribution_indexer import DistributionIndexer
 
 SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'samples')
 CATALOG_ID = 'test_catalog'
@@ -87,3 +81,27 @@ class ReaderTests(TestCase):
         index_catalog(self.node, self.mgmt_task, read_local=True)
 
         self.assertGreater(len(ReadDataJsonTask.objects.get(id=self.task.id).logs), 10)
+
+    def test_index_YYYY_MM_distribution(self, *_):
+        catalog = os.path.join(SAMPLES_DIR, 'single_data_yyyy_mm.json')
+        self.node.catalog_url = catalog
+        self.node.save()
+
+        read_datajson(self.task, whitelist=True, read_local=True)
+        index_catalog(self.node, self.mgmt_task, read_local=True, )
+
+        distribution = Distribution.objects.get(identifier='102.1')
+
+        self.assertEqual(distribution.enhanced_meta.get(key=meta_keys.CHANGED).value, 'True')
+
+    def test_index_YYYY_distribution(self, *_):
+        catalog = os.path.join(SAMPLES_DIR, 'single_data_yyyy.json')
+        self.node.catalog_url = catalog
+        self.node.save()
+
+        read_datajson(self.task, whitelist=True, read_local=True)
+        index_catalog(self.node, self.mgmt_task, read_local=True, )
+
+        distribution = Distribution.objects.get(identifier='102.1')
+
+        self.assertEqual(distribution.enhanced_meta.get(key=meta_keys.CHANGED).value, 'True')
