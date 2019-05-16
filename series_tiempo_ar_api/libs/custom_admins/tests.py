c1a6f72b8adbbcbd4d7b006fda851e2706ac33f3
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-05-16 12:55:57 -0300

    Refactor mocking en tests

--- series_tiempo_ar_api/libs/custom_admins/tests.py
+++ series_tiempo_ar_api/libs/custom_admins/tests.py
@@ -12,6 +12,7 @@ from django_datajsonar.indexing.catalog_reader import index_catalog
 SAMPLES_DIR = os.path.join(os.path.dirname(__file__), 'samples')
 
 
+@mock.patch('series_tiempo_ar_api.libs.custom_admins.admin.delete_metadata')
 class DeleteMetadataTests(TestCase):
     distribution_change = urlresolvers.reverse('admin:django_datajsonar_distribution_changelist')
     field_change = urlresolvers.reverse('admin:django_datajsonar_field_changelist')
@@ -25,21 +26,19 @@ class DeleteMetadataTests(TestCase):
         user = User.objects.create(username='a', password='b', is_staff=True, is_superuser=True)
         self.client.force_login(user)
 
-    def test_delete_fields(self):
-        with mock.patch('series_tiempo_ar_api.libs.custom_admins.admin.delete_metadata') as fake_delete:
-            fields = Field.objects.all()
-            self.client.post(self.field_change, {'action': 'delete_model',
-                                                 ACTION_CHECKBOX_NAME: [str(f.pk) for f in fields]},
-                             follow=True)
+    def test_delete_fields(self, fake_delete):
+        fields = Field.objects.all()
+        self.client.post(self.field_change, {'action': 'delete_model',
+                                             ACTION_CHECKBOX_NAME: [str(f.pk) for f in fields]},
+                         follow=True)
 
-            self.assertEqual(set(fake_delete.call_args[0][0]), set(fields))
+        self.assertEqual(set(fake_delete.call_args[0][0]), set(fields))
 
-    def test_delete_distribution(self):
-        with mock.patch('series_tiempo_ar_api.libs.custom_admins.admin.delete_metadata') as fake_delete:
-            distribution: Distribution = Distribution.objects.filter(dataset__catalog__identifier=self.catalog_id).first()
-            fields = set(distribution.field_set.all())
-            self.client.post(self.distribution_change,
-                             {'action': 'delete_model',
-                              ACTION_CHECKBOX_NAME: [str(distribution.pk)]},
-                             follow=True)
-            self.assertEqual(set(fake_delete.call_args[0][0]), set(fields))
+    def test_delete_distribution(self, fake_delete):
+        distribution: Distribution = Distribution.objects.filter(dataset__catalog__identifier=self.catalog_id).first()
+        fields = set(distribution.field_set.all())
+        self.client.post(self.distribution_change,
+                         {'action': 'delete_model',
+                          ACTION_CHECKBOX_NAME: [str(distribution.pk)]},
+                         follow=True)
+        self.assertEqual(set(fake_delete.call_args[0][0]), set(fields))
