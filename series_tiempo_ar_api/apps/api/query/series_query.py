7be1b28107475833cba57e7f8dd86d763310e869
Lucas Lavandeira <lucaslavandeira@gmail.com>
2019-08-02 12:31:52 -0300

    Agrego comportamiento de metadatos a SeriesQuery

--- series_tiempo_ar_api/apps/api/query/series_query.py
+++ series_tiempo_ar_api/apps/api/query/series_query.py
@@ -1,4 +1,5 @@
 from series_tiempo_ar_api.apps.api.helpers import get_periodicity_human_format
+from series_tiempo_ar_api.apps.api.query.metadata_response import MetadataResponse
 from series_tiempo_ar_api.apps.management import meta_keys
 
 
@@ -12,3 +13,6 @@ class SeriesQuery:
         serie_periodicity = meta_keys.get(self.field_model, meta_keys.PERIODICITY)
         distribution_periodicity = meta_keys.get(self.field_model.distribution, meta_keys.PERIODICITY)
         return get_periodicity_human_format(serie_periodicity or distribution_periodicity)
+
+    def get_metadata(self, flat=False, simple=True):
+        return MetadataResponse(self.field_model, flat=flat, simple=simple).get_response()
--- series_tiempo_ar_api/apps/api/tests/series_query_tests.py
+++ series_tiempo_ar_api/apps/api/tests/series_query_tests.py
@@ -16,3 +16,26 @@ class SeriesQueryTests(TestCase):
     def test_get_periodicity(self):
         periodicity = SeriesQuery(self.field, constants.VALUE).periodicity()
         self.assertEqual(periodicity, 'month')
+
+    def test_get_periodicity_reads_from_distribution_if_serie_has_none(self):
+        self.field.enhanced_meta.all().delete()
+        periodicity = SeriesQuery(self.field, constants.VALUE).periodicity()
+        self.assertEqual(periodicity, 'month')
+
+    def test_get_metadata_has_values_for_all_levels(self):
+        meta = SeriesQuery(self.field, constants.VALUE).get_metadata()
+
+        self.assertIn('catalog', meta)
+        self.assertIn('dataset', meta)
+        self.assertIn('distribution', meta)
+        self.assertIn('field', meta)
+
+    def test_get_metadata_flatten_has_description_in_first_level(self):
+        meta = SeriesQuery(self.field, constants.VALUE).get_metadata(flat=True)
+        self.assertIn('field_description', meta)
+        self.assertNotIn('field', meta)
+
+    def test_enhanced_meta_not_in_metadata_if_query_is_simple(self):
+        meta = SeriesQuery(self.field, constants.VALUE).get_metadata(simple=True)
+
+        self.assertNotIn('available', meta['field'])
