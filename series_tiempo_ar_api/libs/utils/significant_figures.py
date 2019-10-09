2c86298d08bf4b9dc81965ddc220ab27b7a13f28
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-10-09 17:51:28 -0300

    Aplico heurística para solucionar problemas de respresentación decimal en floats

--- series_tiempo_ar_api/libs/utils/significant_figures.py
+++ series_tiempo_ar_api/libs/utils/significant_figures.py
@@ -1,4 +1,5 @@
 import decimal
+import math
 
 import numpy as np
 
@@ -14,7 +15,28 @@ def significant_figures(serie):
     figures = 0
     serie = [x for x in serie if not np.isnan(x)]
     for value in serie:
-        figure = decimal.Decimal(str(value)).normalize().as_tuple().exponent
+        figure = infer_decimals(value)
         figures = max(figures, -figure)
 
     return figures
+
+
+# Adiviná si lo robé de SO
+def truncate(number, digits) -> float:
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+
+def infer_decimals(value):
+    """
+    Devuelve la cantidad de cifras decimales del valor, aplicando una heurística de corrección
+    previa.
+
+    Para valores del estilo 1.0000000000000001, (común al serializar números en punto flotante),
+    se los trunca a 17 - N dígitos, siendo N la cantidad de dígitos enteros del mismo indicador.
+    El objetivo es recortar el margen de error dado por la precisión en floats. De esta manera,
+    el valor del ejemplo anterior, 1.0000000000000001, pasa a 1.000000000000000 -> 1.0
+    """
+    integer_digits = len(str(int(math.modf(value)[1])))
+    truncated = truncate(value, 17 - integer_digits)
+    return decimal.Decimal(str(truncated)).normalize().as_tuple().exponent
