cfa9a2f5babbcfc8d1ae598e0ea66f695cb5c8bf
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-11-26 11:45:39 -0300

    Saco timeout para task de analytics

--- series_tiempo_ar_api/apps/analytics/tasks.py
+++ series_tiempo_ar_api/apps/analytics/tasks.py
@@ -14,12 +14,12 @@ def enqueue_new_import_analytics_task(limit=1000, requests_lib=requests, import_
     AnalyticsImporter(limit=limit, requests_lib=requests_lib, index_to_es=index_to_es).run(import_all)
 
 
-@job('analytics', timeout=1000)
+@job('analytics', timeout=-1)
 def import_analytics(task, limit=1000, requests_lib=requests, import_all=False, index_to_es=True):
     AnalyticsImporter(task=task, limit=limit, requests_lib=requests_lib, index_to_es=index_to_es).run(import_all)
 
 
-@job('hits_indicators', timeout=1000)
+@job('hits_indicators', timeout=-1)
 def enqueue_new_calculate_hits_indicators_task(*_):
     yesterday = datetime.date.today() - relativedelta(days=1)
     calculate_hits_indicators(for_date=yesterday)
